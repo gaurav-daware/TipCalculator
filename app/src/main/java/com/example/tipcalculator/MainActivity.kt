01261ce17cf358bf911e21e0194b40be75f5ff43
Gaurav Daware <gauravdaware02@gmail.com>
2025-03-14 20:20:25 +0530

    added percentage textfield

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -6,15 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -33,6 +37,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tipcalculator.ui.theme.TipCalculatorTheme
 import java.text.NumberFormat
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,8 +58,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TipCalcLayout(){
     var amountInput by remember { mutableStateOf("") }
+    var percentInput by remember { mutableStateOf("") }
     val amount =amountInput.toDoubleOrNull() ?: 0.0
-    val tip = tipcalculator(amount)
+    val tipPercent = percentInput.toDoubleOrNull() ?: 0.0
+    val tip = tipcalculator(amount,tipPercent)
     Column(modifier = Modifier
         .statusBarsPadding()
         .padding(horizontal = 40.dp)
@@ -69,31 +76,38 @@ fun TipCalcLayout(){
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(Alignment.Start)
         )
-        EditNumberField(amountInput,{amountInput=it},modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+        EditNumberField(amountInput,{amountInput=it},"bill amount",modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+
+      //  Spacer(modifier = Modifier.height(4.dp))
+
+        EditNumberField(percentInput,{percentInput=it},"tip percentage",modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
 
 
         Text(text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall)
 
     }
+
 }
 
 @Composable
 fun EditNumberField(value: String,
                     onValueChange: (String) -> Unit = {},
+                    placeholder: String ,
     modifier: Modifier = Modifier){
 
     TextField(
         value = value,
         onValueChange = onValueChange,
         //label = { Text("bill amount") },
-        placeholder = { Text("bill amount") },
+        placeholder = { Text(text = placeholder) },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+      //  leadingIcon = {Icon(Icons.Filled.Person, contentDescription = null)},
         modifier = modifier
     )
 }
 
-private fun tipcalculator(BillAmount: Double, tipPercent: Double=15.0):String {
+private fun tipcalculator(BillAmount: Double, tipPercent: Double):String {
     val tip = tipPercent/100 * BillAmount
     return NumberFormat.getCurrencyInstance().format(tip)
 }
